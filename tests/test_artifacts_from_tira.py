8d3be30f64c5de4d26850508e29849c06d34859d
Maik Fröbe <maik.froebe@uni-jena.de>
2024-08-28 13:15:12 +0200

    Add first test for tira artifacts

---
+++ tests/test_artifacts_from_tira.py
@@ -0,0 +1,28 @@
+import pytest
+import unittest
+import pandas as pd
+from pyterrier_alpha.artifact import from_url
+
+try:
+    import tira
+    TIRA_CLIENT_INSTALLED = True
+except:
+    TIRA_CLIENT_INSTALLED = False
+
+class TestArtifactsFromTira(unittest.TestCase):
+    @pytest.mark.skipif(not TIRA_CLIENT_INSTALLED, reason="This test requires the tira-python client.")
+    def test_foo(self):
+        expected = {
+            'qid': '1',
+            'docno': 'clueweb12-0200wb-79-18105',
+            'name': 'sparse_cross_encoder',
+            'rank': 1,
+            'score': 8.310153007507324 
+        }
+        sparse_cross_encoder_transformer = from_url('tira://clueweb12/touche-2020-task-2/fschlatt/sparse-cross-encoder-4-512')
+        actual = sparse_cross_encoder_transformer(pd.DataFrame([{'qid': '1'}]))
+	
+        self.assertEqual(expected, actual.iloc[0].to_dict())
+
+    # TODO: Add more tests, also for different types
+
